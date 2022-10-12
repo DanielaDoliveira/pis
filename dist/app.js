1d96fad0eebea2b46c095fa0a9ebf5d20d2baacd
Daniela Fialho D Oliveira <danielafialhodoliveira@iMac-de-Daniela.local>
2022-10-12 16:26:20 -0300

    U6

--- dist/app.js
+++ dist/app.js
@@ -3,13 +3,14 @@ var _routes = require('./routes'); var _routes2 = _interopRequireDefault(_routes
 var _mongoose = require('mongoose'); var _mongoose2 = _interopRequireDefault(_mongoose);
 var _dotenv = require('dotenv'); var dotenv = _interopRequireWildcard(_dotenv);
 var _bodyparser = require('body-parser'); var _bodyparser2 = _interopRequireDefault(_bodyparser);
+var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
 dotenv.config()
 
 
 class App{
   constructor(){
     this.server = _express2.default.call(void 0, );
-    _mongoose2.default.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@projetointegrado.hhlgxz0.mongodb.net/?retryWrites=true&w=majority`)
+    _mongoose2.default.connect(`mongodb+srv://projetointegrado:projetointegrado@projetointegrado.hhlgxz0.mongodb.net/?retryWrites=true&w=majority`)
     this.middleware();
     this.routes();
 
@@ -23,7 +24,11 @@ class App{
     this.server.use(_routes2.default);
   }
 
+
+ 
+
 }
+
 
 
 
