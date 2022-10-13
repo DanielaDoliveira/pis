c598eeab099a97eab21cd35acbcf28be5975c00d
Daniela Fialho D Oliveira <danielafialhodoliveira@iMac-de-Daniela.local>
2022-10-13 12:41:23 -0300

    Página raiz da API

--- dist/controllers/RootController.js
+++ dist/controllers/RootController.js
@@ -1,10 +1,12 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true});
+"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+var _path = require('path'); var _path2 = _interopRequireDefault(_path);
 
+_path2.default = require('path');
 class RootController{
  async index(req,res){
 
-  return res.send('<h1>Welcome to the Root Page</h1>');
+  return res.sendFile(_path2.default.resolve(__dirname,'..','Files','information.html'));
 }
 
 }
